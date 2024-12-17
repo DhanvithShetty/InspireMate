e0c94f4c750b0066521d2b9f63a1151cfa64925a
Dhanvith Shetty <dhanvithshetty02@gmail.com>
2024-12-18 00:33:47 +0530

    Added green background and corresponding font colour

--- Designs.py
+++ Designs.py
@@ -1,7 +1,7 @@
 # Dictionary mapping background images to font colors
 Backgrounds_And_Font_Colours = {
     "Backgrounds/Black.png": "#E1C340",
-    #"Backgrounds/Green.png": "#",
+    "Backgrounds/Green.png": "#2A2C2B",
     "Backgrounds/Grey.png": "#3D3B3A",
     "Backgrounds/Navy_Blue.png": "#D7BB6F",
     "Backgrounds/Peach.png": "#C48B55"
