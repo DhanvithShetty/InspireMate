8a5a4abe538351ef634b198df4a366ee3823134d
Dhanvith Shetty <dhanvithshetty02@gmail.com>
2024-12-19 10:25:24 +0530

    Changed code to use instagrapi instead of instabot and updated requirements.txt

--- Upload.py
+++ Upload.py
@@ -1,6 +1,5 @@
-from instabot import Bot
+from instagrapi import Client
 import os
-import shutil
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -14,23 +13,9 @@ def UploadPost(image_path, caption):
     # Ensure the credentials exist
     if not username or not password:
         raise ValueError("Instagram username or password is missing")
-
-    # Remove the 'config' folder if it exists
-    if os.path.exists("config"):
-        shutil.rmtree("config")
-        print("Old config folder deleted.")
-
-    # Ensure no conflicts with the 'REMOVE_ME' file
-    remove_me_file = f"{image_path}.REMOVE_ME"
-    if os.path.exists(remove_me_file):
-        os.remove(remove_me_file)
-        print(f"Removed existing file: {remove_me_file}")
     
-    # Initialize the bot
-    bot = Bot()
+    cl = Client()
+    cl.login(username, password)
 
-    # Login to Instagram (using environment variables)
-    bot.login(username=username, password=password)
-
-    # Upload an image to Instagram (provide the path to your image)
-    bot.upload_photo(image_path, caption=caption)
+    cl.photo_upload(image_path, caption)
+    print("Post uploaded successfully")
